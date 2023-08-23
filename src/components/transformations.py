e366f7b6ed3020c14d822de3f45d7c11209e9faf
Vitor Leitao <vml2@cin.ufpe.br>
2023-08-23 14:18:03 -0300

    Alterações nas transformações afins

--- src/components/transformations.py
+++ src/components/transformations.py
@@ -85,49 +85,73 @@ class Transformable:
             ]
         )
 
-    def rotate(
-        self,
-        point: src.components.point.Point,
-        vector: src.components.vector.Vector,
-        angle_radians: float | None = None,
-        angle_degrees: float | None = None,
-    ) -> Transformable:
-        """Returns the object rotated by the given point, vector, and angle. The angle must be specified in either radians or degrees."""
-        if angle_radians is not None and angle_degrees is not None:
-            raise ValueError(
-                "Only one of angle_radians or angle_degrees can be specified."
-            )
-
-        if angle_degrees is not None:
-            angle_radians = math.radians(angle_degrees)
-        elif angle_radians is None:
-            raise ValueError("Either angle_radians or angle_degrees must be specified.")
-
+    def rotate(self, point: src.components.point.Point, vector: src.components.vector.Vector, angle: float) -> any:
+        """Return the object after being rotated by angle(degrees) around the axis defined by a point and a vector clockwise"""
+        # Convert angle from degrees to radians
+        angle = math.radians(angle)
+        
+        # Normalize the axis vector
         axis = vector.normalized()
-        cos = math.cos(angle_radians)
-        sin = math.sin(angle_radians)
+        
+        # Calculate the rotation matrix
+        cos_a = math.cos(angle)
+        sin_a = math.sin(angle)
+        ux = axis.x
+        uy = axis.y
+        uz = axis.z
+        rot_matrix = [
+            [cos_a + ux**2*(1-cos_a), ux*uy*(1-cos_a) - uz*sin_a, ux*uz*(1-cos_a) + uy*sin_a, 0],
+            [uy*ux*(1-cos_a) + uz*sin_a, cos_a + uy**2*(1-cos_a), uy*uz*(1-cos_a) - ux*sin_a, 0],
+            [uz*ux*(1-cos_a) - uy*sin_a, uz*uy*(1-cos_a) + ux*sin_a, cos_a + uz**2*(1-cos_a), 0],
+            [0, 0, 0, 1]]
 
-        rotation_matrix = [
-            [
-                cos + axis.x * axis.x * (1 - cos),
-                axis.x * axis.y * (1 - cos) - axis.z * sin,
-                axis.x * axis.z * (1 - cos) + axis.y * sin,
-                0,
-            ],
-            [
-                axis.y * axis.x * (1 - cos) + axis.z * sin,
-                cos + axis.y * axis.y * (1 - cos),
-                axis.y * axis.z * (1 - cos) - axis.x * sin,
-                0,
-            ],
-            [
-                axis.z * axis.x * (1 - cos) - axis.y * sin,
-                axis.z * axis.y * (1 - cos) + axis.x * sin,
-                cos + axis.z * axis.z * (1 - cos),
-                0,
-            ],
-            [0, 0, 0, 1],
+        to_origin_matrix = [
+            [1, 0, 0, -point.x],
+            [0, 1, 0, -point.y],
+            [0, 0, 1, -point.z], 
+            [0, 0, 0, 1]]
+
+        back_from_origin_matrix = [
+            [1, 0, 0, point.x],
+            [0, 1, 0, point.y],
+            [0, 0, 1, point.z], 
+            [0, 0, 0, 1]]
+
+        def matrix_multiply(A, B):
+            m = len(A)
+            n = len(B[0])
+            product = []
+            for i in range(m):
+                row = []
+                for j in range(n):
+                    element = 0
+                    for k in range(len(B)):
+                        element += A[i][k] * B[k][j]
+                    row.append(element)
+                product.append(row)
+            return product
+
+        final_matrix = matrix_multiply(back_from_origin_matrix, matrix_multiply(rot_matrix, to_origin_matrix))
+
+        return self.transform(final_matrix)
+
+    def scale(self, vector) -> any:
+        distotion_matrix = [
+            [vector.x, 0, 0, 0],
+            [0, vector.y, 0, 0],
+            [0, 0, vector.z, 0],
+            [0, 0, 0, 1]
         ]
+        return self.transform(distotion_matrix)
 
-        # TODO: Implement this method
-        raise NotImplementedError("This method is not yet fully implemented.")
+    def reflect(self, point, normal) -> any:
+        d = -(normal ^ point)
+        normal = normal
+        reflection_matrix = [
+            [1 - 2 * normal.x ** 2, 0 - 2 * normal.x * normal.y, 0 - 2 * normal.x * normal.z, -2 * d * normal.x],
+            [- 2 * normal.x * normal.y, 1 - 2 * normal.y **2, 0 - 2 * normal.y * normal.z, -2 * d * normal.y],
+            [0 - 2 * normal.x * normal.z, 0 - 2 * normal.z * normal.y, 1 - 2 * normal.z ** 2, -2 * d * normal.z],
+            [0, 0, 0, 1]
+        ]
+        return self.transform(reflection_matrix)
+
